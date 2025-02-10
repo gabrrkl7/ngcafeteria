8958915e620bf2dc13c2e85a9493a9b4cedb1dea
Gabriel <gabriel.dev09@gmail.com>
2025-02-10 16:43:53 -0300

    Criação Header, Footer, Main e início criação de formulário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { CriarPedidoComponent } from './componentes/pedidos/criar-pedido/criar-pedido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    CriarPedidoComponent
   ],
   imports: [
     BrowserModule,
